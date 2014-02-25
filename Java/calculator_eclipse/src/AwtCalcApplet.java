bbff35c756665f74ad6ae3dd1da9c1847e582085
Dennis Ruhe <dennis@amazingsystems.nl>
2014-02-25 11:55:01 +0100

    Imported calculator_eclipse

---
+++ Java/calculator_eclipse/src/AwtCalcApplet.java
@@ -0,0 +1,21 @@
+package calc;
+/*
+*  File:   		AwtCalcApplet.java
+*  Description: Allow's AwtCalc to be run as an applet.
+*/
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class AwtCalcApplet extends Applet {
+
+public void init()
+{
+
+setLayout( new BorderLayout() );
+add( new AwtCalc(), BorderLayout.CENTER );
+
+}
+
+
+}
